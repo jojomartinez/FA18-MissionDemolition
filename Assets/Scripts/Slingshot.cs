3095c4aa758b37eff4af341e8e45fda91e4693e6
jojomartinez <jojomartinez@github.com>
2018-11-22 11:30:18 -0600

    set halo to activate and slingshot to a certain velocity

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+    //fields set in the unity inspector pane
+    [Header("Set in inspector")]
+    public GameObject prefabProjectile;
+    public float velocityMult = 8f;
+
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    private Rigidbody projectileRigidbody;
+
+
+    private void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
+
+    // Use this for initialization
+    void OnMouseEnter() {
+        //print("Slingshot:OnMouseEnter()"); 523
+        launchPoint.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void OnMouseExit() {
+        // print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+
+    }
+
+    void OnMouseDown()
+    {
+        //the players perssed tghe nmouse button while sling shot
+        aimingMode = true;
+        //insitantiate a projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        //start it at the launchpoint
+        projectile.transform.position = launchPos;
+        //set it to iskinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
+    }
+
+    void Update()
+    {
+        //if slingshot is not in aimingmode, dont run this code
+        if (!aimingMode) return;
+
+        //get the current mouse position in 2d  sreen coodirnates 
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //find the delta from the laucnhhpos to the mousepose3d
+        Vector3 mouseDelta = mousePos3D - launchPos;
+
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude<maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+        Vector3 projPos = launchPos + mouseDelta;
+
+        projectile.transform.position = projPos;
+
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+        }
+
+        projectileRigidbody.isKinematic = false;
+        projectileRigidbody.velocity = -mouseDelta * velocityMult;
+        projectile = null;
+
+            }
+}
